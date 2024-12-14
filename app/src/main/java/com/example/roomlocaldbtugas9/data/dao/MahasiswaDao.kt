d7ab3f6b7ccf369de4cfa210f9b894f04e95c3b2
dinaamlll <dinamaliaa17@gmail.com>
2024-12-14 16:01:38 +0700

    menambahkan getAllMahasiswa untuk mengambil semua data mahasiswa yang tersimpan di database

--- app/src/main/java/com/example/roomlocaldbtugas9/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomlocaldbtugas9/data/dao/MahasiswaDao.kt
@@ -2,10 +2,16 @@ package com.example.roomlocaldbtugas9.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.roomlocaldbtugas9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao //Dao buat DML
 interface MahasiswaDao {
     @Insert
     suspend fun  insertMahasiswa(mahasiswa: Mahasiswa)
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+
+
 }
