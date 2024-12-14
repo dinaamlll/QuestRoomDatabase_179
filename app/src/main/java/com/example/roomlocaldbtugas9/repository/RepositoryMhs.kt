5d7541c32bb70b668173cdc9360a193799d34669
dinaamlll <dinamaliaa17@gmail.com>
2024-12-14 16:14:38 +0700

    menambahkan getAllMhs (mendapatkan semua data mhs), getMhs(mengambil data mhs berdasarkan nim), deleteMhs, dan updateMhs

--- app/src/main/java/com/example/roomlocaldbtugas9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldbtugas9/repository/RepositoryMhs.kt
@@ -1,7 +1,13 @@
 package com.example.roomlocaldbtugas9.repository
 
 import com.example.roomlocaldbtugas9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs() : Flow<List<Mahasiswa>>
+    fun getMhs(nim: String) : Flow<Mahasiswa>
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    suspend fun  updateMhs(mahasiswa: Mahasiswa)
 }
