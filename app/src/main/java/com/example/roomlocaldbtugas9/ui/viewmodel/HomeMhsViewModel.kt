177d3640ee0772a83ccc7fca093f8ce9a319b312
dinaamlll <dinamaliaa17@gmail.com>
2024-12-15 14:58:37 +0700

    membuat halaman Home untuk menampilkan data mahasiswa secara real-time dengan status yang selalu diperbarui, seperti loading, data siap, atau error, menggunakan StateFlow.

--- app/src/main/java/com/example/roomlocaldbtugas9/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldbtugas9/ui/viewmodel/HomeMhsViewModel.kt
@@ -14,9 +14,39 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 
-class HomeMhsViewModel {
-
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel(){
+    val homeUIState : StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
 }
+
 data class HomeUiState(
     val listMhs :List<Mahasiswa> = listOf(),
     val isLoading : Boolean = false,
