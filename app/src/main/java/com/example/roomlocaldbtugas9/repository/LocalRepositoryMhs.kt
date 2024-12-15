6c42a466ab3a3f5614a065ae4869fe3f94fde203
dinaamlll <dinamaliaa17@gmail.com>
2024-12-15 14:49:00 +0700

    menambahkan getAllMhs, getMhs, deleteMhs, dan updateMhs di LocalRepositoryMhs

--- app/src/main/java/com/example/roomlocaldbtugas9/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldbtugas9/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,25 @@ package com.example.roomlocaldbtugas9.repository
 
 import com.example.roomlocaldbtugas9.data.dao.MahasiswaDao
 import com.example.roomlocaldbtugas9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
-    override suspend fun insertMhs(mahasiswa: Mahasiswa){
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+    //getAllMhs
+    override fun getAllMhs() : Flow<List<Mahasiswa>>{
+        return mahasiswaDao.getAllMahasiswa()
     }
+    override fun getMhs(nim: String) : Flow<Mahasiswa>{
+        return mahasiswaDao.getMhs(nim)
+    }
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa){
+        mahasiswaDao.deleteMhs(mahasiswa)
+    }
+    override suspend fun  updateMhs(mahasiswa:Mahasiswa){
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
+}
