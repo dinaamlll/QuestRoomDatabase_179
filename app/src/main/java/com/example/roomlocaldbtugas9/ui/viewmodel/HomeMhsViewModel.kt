9cbc98459729194975b59440b4d3e2a010c85b94
dinaamlll <dinamaliaa17@gmail.com>
2024-12-15 14:29:18 +0700

    membuat data class HomeUiState yang isi nya ada val listMhs (untuk nyimpen daftar mahasiswa), isLoading (Untuk tahu lagi proses loading atau ngga), isError (untuk tahu ada error atau nggak), errorMessage (untuk nyimpen pesan error kalau ada masala)

---
+++ app/src/main/java/com/example/roomlocaldbtugas9/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.roomlocaldbtugas9.ui.viewmodel
+
+import androidx.core.app.NotificationCompat.MessagingStyle.Message
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomlocaldbtugas9.data.entity.Mahasiswa
+import com.example.roomlocaldbtugas9.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeMhsViewModel {
+
+}
+data class HomeUiState(
+    val listMhs :List<Mahasiswa> = listOf(),
+    val isLoading : Boolean = false,
+    val isError : Boolean = false,
+    val errorMessage: String = ""
+)
