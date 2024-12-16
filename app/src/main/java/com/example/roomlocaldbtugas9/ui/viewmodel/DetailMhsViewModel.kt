e3c8501e18869f4a21554803a75fb0064d0ed3f3
dinaamlll <dinamaliaa17@gmail.com>
2024-12-16 16:43:51 +0700

    Menambahkan `StateFlow` untuk pengambilan data detail mahasiswa berdasarkan NIM menggunakan `savedStateHandle[DestinasiDetail.NIM]` dan tidak null serta menambahkan `catch` utk menampilkan pesan kesalahan jika terjadi error saat pengambilan data.

--- app/src/main/java/com/example/roomlocaldbtugas9/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldbtugas9/ui/viewmodel/DetailMhsViewModel.kt
@@ -23,6 +23,37 @@ class DetailMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 ) : ViewModel() {
+    private val _nim : String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = true,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+
+                    )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
 
 
     fun deleteMhs() {
