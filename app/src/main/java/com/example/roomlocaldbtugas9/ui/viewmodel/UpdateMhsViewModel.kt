56252b1b996a580479a9bbf5ceeaf08ecdadc977
dinaamlll <dinamaliaa17@gmail.com>
2024-12-16 22:40:45 +0700

    Menambahkan ViewModel untuk memperbarui data mahasiswa

---
+++ app/src/main/java/com/example/roomlocaldbtugas9/ui/viewmodel/UpdateMhsViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.roomlocaldbtugas9.ui.viewmodel
+
+class UpdateMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel(){
+    var updateUIState by mutableStateOf(MhsUIState())
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init{
+        viewModelScope.launch{
+            updateUIState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
